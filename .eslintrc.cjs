7444597b37d6a73848087c4f33a27f04bc1b54c4
inx-rj <rahul@inexture.com>
2023-07-01 15:55:44 +0530

    Setup of prerequisites dependencies and configs for the same

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,24 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+    root: true,
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:import/recommended',
+        'standard-with-typescript',
+        'plugin:@typescript-eslint/recommended',
+        // Disables the ESLint rules which will be handled by the "prettier"
+        'eslint-config-prettier' // Always keep it last to overide the other configs
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {}
+};
